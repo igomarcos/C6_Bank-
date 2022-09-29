1b39a96e4f149976b4350dc5a2c4ca8ef1d2a54b
Igo Marcos <igo.marcos.da.silva@avanade.com>
2022-09-29 13:27:19 -0300

    'Deploy'

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+console.log (nome, "chamando a variavel usando o var");
+
+var nome = "avanade";
+
+
+// existem 7 tipos de erros no javascript
+
+// SyntaxError - escreveu alguma coisa errado
+// ReferenceError - chamou alguém que não existe
+// TypeError - erro de tipo é uma violação do javascript
+// tokenização - const nome = valor;
